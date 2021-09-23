d77259f6029bda0a4a97b3ae0427f2e07141ff36
Melynda Slytherin <mslyther@mi-v1.msk.21-school.ru>
2021-09-23 19:20:24 +0300

    improved replace.c

--- sort/replace.c
+++ sort/replace.c
@@ -1,5 +1,17 @@
 #include "push_swap.h"
 
+int	check_existence(t_stack *comms, int current, int next)
+{
+	return (comms->content == current && comms->next->content == next);
+}
+
+void	replacement(t_stack	**comms, t_stack *save, int comm)
+{
+	(*comms)->content = comm;
+	ft_lstdel_next(comms);
+	*comms = save;
+}
+
 void	replace(t_stack	**comms)
 {
 	t_stack	*save;
@@ -7,28 +19,15 @@ void	replace(t_stack	**comms)
 	save = *comms;
 	while (*comms && (*comms)->next)
 	{
-		if (((*comms)->content == SA && (*comms)->next->content == SB)
-			|| ((*comms)->content == SB && (*comms)->next->content == SA))
-		{
-			(*comms)->content = SS;
-			ft_lstdel_next(comms);
-		}
-		if (((*comms)->content == RA && (*comms)->next->content == RB)
-			|| ((*comms)->content == RB && (*comms)->next->content == RA))
-		{
-			(*comms)->content = RR;
-			ft_lstdel_next(comms);
-		}
-		if (((*comms)->content == RRA && (*comms)->next->content == RRB)
-			|| ((*comms)->content == RRB && (*comms)->next->content == RRA))
-		{
-			(*comms)->content = RRR;
-			ft_lstdel_next(comms);
-		}	
-		if ((*comms)->next->next && (((*comms)->next->content == PA
-					&& (*comms)->next->next->content == PB)
-				|| ((*comms)->next->content == PB
-					&& (*comms)->next->next->content == PA)))
+		if (check_existence(*comms, SA, SB) || check_existence(*comms, SB, SA))
+			replacement(comms, save, SS);
+		if (check_existence(*comms, RA, RB) || check_existence(*comms, RB, RA))
+			replacement(comms, save, RR);
+		if (check_existence(*comms, RRA, RRB)
+			|| check_existence(*comms, RRB, RRA))
+			replacement(comms, save, RRR);
+		if ((*comms)->next->next && (check_existence((*comms)->next, PA, PB)
+				|| check_existence((*comms)->next, PB, PA)))
 		{
 			ft_lstdel_next(comms);
 			ft_lstdel_next(comms);
